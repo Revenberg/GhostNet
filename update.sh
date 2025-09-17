abf040a9a18cba267d283037d886b119abf2ddc8
Revenberg <sander@revenberg.info>
2025-09-17 12:18:23 +0200

    docker fix

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/Revenberg/ghostnet/main/setup.sh; chmod +x setup.sh; ./setup.sh
+curl -H "Cache-Control: no-cache, no-store, must-revalidate Pragma: no-cache Expires: 0" -O https://raw.githubusercontent.com/Revenberg/ghostnet/main/setup.sh; chmod +x setup.sh; ./setup.sh
