2d1741ff5dcec7fc8d6a08ca24e4145e5341bd30
Revenberg <sander@revenberg.info>
2025-09-19 20:21:41 +0200

    .

--- docker/ghostnet-web/src/pages/Games/GamesProgressOverview.jsx
+++ docker/ghostnet-web/src/pages/Games/GamesProgressOverview.jsx
@@ -92,10 +92,3 @@ export default function GamesProgressOverview() {
     </RequireRole>
   );
 }
-
-// Helper functie voor datum/tijd formaat
-function formatDateTime(ts) {
-  const d = new Date(ts);
-  const pad = n => n.toString().padStart(2, '0');
-  return `${pad(d.getDate())}-${pad(d.getMonth() + 1)}-${d.getFullYear()} ${pad(d.getHours())}:${pad(d.getMinutes())}`;
-}
