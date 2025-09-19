b67f69f539aeeb3cbcece7d8ac19ef316697db7a
Revenberg <sander@revenberg.info>
2025-09-19 12:57:36 +0200

    .

--- docker/backend/routes/teams.js
+++ docker/backend/routes/teams.js
@@ -1,8 +1,6 @@
 import express from "express";
 
 export default function createTeamsRouter(pool) {
-
-
   const router = express.Router();
   // Create a new team event
   router.post("/events", async (req, res) => {
