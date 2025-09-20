a6c3c6019609d57a249ef6c7a5308815c41f0f84
Revenberg <sander@revenberg.info>
2025-09-20 08:42:51 +0200

    .

--- docker/backend/init-db.js
+++ docker/backend/init-db.js
@@ -12,11 +12,11 @@ export async function ensureTables(pool) {
   // removed duplicate game table creation above
 
   await conn.query(`
-    drop table if exists game_route_points;
+    drop table if exists game_routes;
     `);
 
-      await conn.query(`
-    drop table if exists game_routes;
+    await conn.query(`
+    drop table if exists game_route_points;
     `);
 
   await conn.query(`
