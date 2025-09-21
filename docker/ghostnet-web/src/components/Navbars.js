9d21fdf05069bdbc3ac80e5dcf73d2e54709aff9
Revenberg <sander@revenberg.info>
2025-09-21 19:22:01 +0200

    Update NavbarAdmin to link to Games management page

--- docker/ghostnet-web/src/components/Navbars.js
+++ docker/ghostnet-web/src/components/Navbars.js
@@ -73,8 +73,7 @@ export function NavbarAdmin() {
       <div className="relative group">
         <button className="hover:underline focus:outline-none">Games voorbereiding ▾</button>
         <div className="absolute left-0 mt-2 w-56 bg-white text-black rounded shadow-lg opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity z-10">
-          <Link to="/games-list" className="block px-4 py-2 hover:bg-purple-100">Alle games</Link>
-          <Link to="/games-create" className="block px-4 py-2 hover:bg-purple-100">Game aanmaken</Link>
+          <Link to="/games-manage" className="block px-4 py-2 hover:bg-purple-100">Games beheren</Link>
           <Link to="/games-route-points" className="block px-4 py-2 hover:bg-purple-100">Route punten beheren</Link>
           <Link to="/games/create-route" className="block px-4 py-2 hover:bg-purple-100">Route aanmaken/beheren</Link>
           <Link to="/games/all-routes-map" className="block px-4 py-2 hover:bg-purple-100">Kaart: alle routes</Link>
