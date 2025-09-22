6eac5298f4b99e9bf704eb0c2cc9c02d8d1568f2
Revenberg <sander@revenberg.info>
2025-09-22 11:06:26 +0200

    Refactor NavbarAdmin component to enhance user management dropdown functionality

--- docker/ghostnet-web/src/components/Navbars.js
+++ docker/ghostnet-web/src/components/Navbars.js
@@ -109,17 +109,25 @@ export function NavbarOperator() {
 export function NavbarAdmin() {
   const user = getUserFromCookie();
 
+  const [userMgmtDropdownOpen, setUserMgmtDropdownOpen] = React.useState(false);
   return (
     <nav className="bg-purple-700 text-white p-4 flex items-center justify-center space-x-6">
       <Link to="/" className="hover:underline">Home</Link>
       <Link to="/speluitleg" className="hover:underline">Speluitleg</Link>
       <Link to="/contact" className="hover:underline">Contact</Link>
-      <div className="relative group">
-        <button className="hover:underline focus:outline-none">Gebruikersbeheer ▾</button>
-        <div className="absolute left-0 mt-2 w-48 bg-white text-black rounded shadow-lg opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity z-10">
-          <Link to="/users-register" className="block px-4 py-2 hover:bg-purple-100">Register</Link>
-          <Link to="/update-user" className="block px-4 py-2 hover:bg-purple-100">Profiel bijwerken</Link>
-        </div>
+      <div className="relative">
+        <button
+          className="hover:underline focus:outline-none"
+          onClick={() => setUserMgmtDropdownOpen((open) => !open)}
+        >
+          Gebruikersbeheer ▾
+        </button>
+        {userMgmtDropdownOpen && (
+          <div className="absolute left-0 mt-2 w-48 bg-white text-black rounded shadow-lg z-10">
+            <Link to="/users-register" className="block px-4 py-2 hover:bg-purple-100">Register</Link>
+            <Link to="/update-user" className="block px-4 py-2 hover:bg-purple-100">Profiel bijwerken</Link>
+          </div>
+        )}
       </div>
 
       <div className="relative group">
